20f4e39a5f300a35372c6981b0d9604810374231
Sachin Negi <sachin@moolcode.com>
2020-10-26 22:02:47 +0530

    filter checkboxes added

--- cards.js
+++ cards.js
@@ -3,6 +3,7 @@ const url =
 const cardsContainer = document.getElementById("cards_container");
 const cards_section = document.getElementById("cards_section");
 let data = [];
+let filteredData = []
 window.onload = fetchData;
 
 function fetchData() {
@@ -10,12 +11,13 @@ function fetchData() {
     .then((res) => res.json())
     .then((result) => {
       data = result.feed.entry;
-      console.log(data);
+      filteredData = data;
+      //console.log(data);
       setCards();
       cards_section.style.display = "block";
     })
     .catch((err) => {
-      console.log(err);
+      //console.log(err);
     });
 }
 
@@ -25,7 +27,7 @@ function parseKey(obj, key) {
 
 function setCards() {
   let temp = "";
-  data.forEach((e) => {
+  filteredData.forEach((e) => {
     temp += ` <div class="parentcardbox col-sm-6 col-md-4 col-lg-3"  data-aos="zoom-in">
    <div class="card">
      <h5 class="mb-3">${parseKey(e, "name")}</h5>
@@ -55,4 +57,37 @@ function setCards() {
   });
 
   cardsContainer.innerHTML = temp;
+}
+
+function handleCheckbox(e) {
+  const {
+    name,
+    checked
+  } = e.target;
+  const allCheckbox = document.getElementById("checkbox-for-all")
+  const wonCheckbox = document.getElementById("checkbox-for-wononly")
+  const internCheckbox = document.getElementById("checkbox-for-internationonly")
+  const presCheckbox = document.getElementById("checkbox-for-prestigious")
+
+  //console.log(name, checked)
+  allCheckbox.checked = false;
+  wonCheckbox.checked = false;
+  internCheckbox.checked = false;
+  presCheckbox.checked = false;
+  if (name === "all" && checked === true) {
+    allCheckbox.checked = true
+    filteredData = data
+  } else if (name === "wononly" && checked === true) {
+    wonCheckbox.checked = true;
+    filteredData = data.filter(i => parseKey(i, "wonlost") === "W")
+  } else if (name === "internationonly" && checked === true) {
+    internCheckbox.checked = true;
+    filteredData = data.filter(i => parseKey(i, "nation") === "I")
+  } else if (name === "prestigious" && checked === true) {
+    presCheckbox.checked = true;
+    filteredData = data.filter(i => parseKey(i, "prestigious") === "Y")
+  }
+
+  //console.log(filteredData)
+  setCards()
 }
